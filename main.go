52873be9937ff4d3a0031a7264f10c96b6994186
Alexandra Saganelidze <alekarah.all@gmail.com>
2025-03-22 20:32:28 +0300

    second iteration

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -47,21 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "invalid status code")
-
-	body := responseRecorder.Body.String()
-	assert.NotEmpty(t, body, "body is empty")
-
-	list := strings.Split(body, ",")
-	assert.Len(t, list, totalCount, "incorrect len")
 }
